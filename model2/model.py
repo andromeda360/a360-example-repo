eac7142f6d56a8e895b750a4aedfecad504b57ec
Hongwei Tian <hongwei.tian@hypergiant.com>
2021-12-09 14:43:17 -0600

    update example prediction script

--- model2/model.py
+++ model2/model.py
@@ -1,8 +1,11 @@
+
 import json
 import numpy as np
 
-def predict(compiled_model, payload):
+def predict(artifact_list, payload):
+
     payload_dict = json.loads(payload)
+    model = artifact_list[0]
 
     feature_names = [
         "ad_id",
@@ -19,6 +22,6 @@ def predict(compiled_model, payload):
     prediction_list = [payload_dict[feature] for feature in feature_names]
     prediction_vector = np.array(prediction_list).reshape(1, -1)
 
-    prediction = compiled_model.predict(prediction_vector)
+    prediction = model.predict(prediction_vector)
 
-    return prediction[0]
+    return float(prediction[0])
